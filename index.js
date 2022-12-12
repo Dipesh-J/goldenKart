ce46f8ff62eeb5fdc6b04e981d71013ebf7e4c39
Dipesh-J <dipeshjoshi015@gmail.com>
2022-12-12 12:55:10 +0530

    ok

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use("/",route)
 
 mongoose.connect("mongodb+srv://group22:1234@group22databse.uvtoalh.mongodb.net/goldenKartDB",{useNewUrlParser:true})
+
 const PORT = 7777
 
 app.listen(PORT, ()=>{
